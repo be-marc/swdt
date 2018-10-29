94399595f97f800a83589d16e54fb7594aa8cd0b
Marc Becker <marcbecker@posteo.de>
2018-10-29 13:27:50 +0100

    Fix hole bug in png files

--- swdt/ utilities/tif_to_png.R
+++ swdt/ utilities/tif_to_png.R
@@ -6,8 +6,8 @@ library(rgdal)
 library(glue)
 
 # Config
-in_path <- "./data/fuente"
-out_path <- "./www/thumb/fuente/"
+in_path <- "./swdt/data/fuente"
+out_path <- "./swdt/www/thumb/fuente/"
 
 paths <- list.files(in_path, "^S1", full.names = TRUE)
 names <- list.files(in_path, "^S1")
@@ -16,7 +16,9 @@ rgdal::setCPLConfigOption("GDAL_PAM_ENABLED", "FALSE")
 
 for(i in 1:length(paths)) {
   raster(paths[i]) %>%
-    stretch(minq=0.05, maxq=0.95) %>%
+    stretch(minq=0.05, maxq=0.95) -> r 
+    r[r == 255] <- 254
+    r %>%
     as('SpatialPixelsDataFrame') %>%
     rgdal::writeGDAL(glue(out_path, file_path_sans_ext(names[i]), ".png"),
                      drivername = "PNG", type = "Byte", mvFlag = 255) 
